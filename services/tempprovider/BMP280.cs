72ae3c765003a0a24521b430838446be71323814
LokiFaun <johannes.schuetz@outlook.com>
2015-10-01 23:13:51 +0200

    added tempprovider
    - initial commit

---
+++ services/tempprovider/BMP280.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using Windows.Devices.I2c;
+
+namespace tempprovider
+{
+    public sealed class BMP280
+    {
+        private const int REGISTER_DIG_T1 = 0x88;
+        private const int REGISTER_DIG_T2 = 0x8A;
+        private const int REGISTER_DIG_T3 = 0x8C;
+
+        private const int REGISTER_DIG_P1 = 0x8E;
+        private const int REGISTER_DIG_P2 = 0x90;
+        private const int REGISTER_DIG_P3 = 0x92;
+        private const int REGISTER_DIG_P4 = 0x94;
+        private const int REGISTER_DIG_P5 = 0x96;
+        private const int REGISTER_DIG_P6 = 0x98;
+        private const int REGISTER_DIG_P7 = 0x9A;
+        private const int REGISTER_DIG_P8 = 0x9C;
+        private const int REGISTER_DIG_P9 = 0x9E;
+
+        private const int REGISTER_CHIPID = 0xD0;
+        private const int REGISTER_VERSION = 0xD1;
+        private const int REGISTER_SOFTRESET = 0xE0;
+
+        private const int REGISTER_CAL26 = 0xE1;
+
+        private const int REGISTER_CONTROL = 0xF4;
+        private const int REGISTER_CONFIG = 0xF5;
+        private const int REGISTER_PRESSUREDATA = 0xF7;
+        private const int REGISTER_TEMPDATA = 0xFA;
+
+        private struct CalibrationData
+        {
+            public ushort T1;
+            public short T2;
+            public short T3;
+
+            public ushort P1;
+            public short P2;
+            public short P3;
+            public short P4;
+            public short P5;
+            public short P6;
+            public short P7;
+            public short P8;
+            public short P9;
+
+            public ushort H1;
+            public short H2;
+            public ushort H3;
+            public short H4;
+            public short H5;
+            public short H6;
+        }
+
+        private int m_Normalized = 0;
+        private I2cDevice m_Device;
+        private CalibrationData m_Calibration = new CalibrationData();
+
+        public static int Address { get; } = 0x76;
+
+        /// <summary>
+        /// Initializes the sensor
+        /// </summary>
+        /// <param name="device">The I2C device for communication with the sensor</param>
+        public BMP280(I2cDevice device)
+        {
+            m_Device = device;
+        }
+
+        /// <summary>
+        /// Powers up the sensor
+        /// </summary>
+        /// <returns>false in case of error, otherwise false</returns>
+        public bool PowerUp()
+        {
+            if (Read8(REGISTER_CHIPID) != 0x58)
+            {
+                return false;
+            }
+            ReadCoefficients();
+            Write8(REGISTER_CONTROL, 0x3F);
+            return true;
+        }
+
+        /// <summary>
+        /// Read the temperature from the sensor
+        /// </summary>
+        /// <returns>The temperature in °C</returns>
+        public double ReadTemperature()
+        {
+            int adcTemperature = Read16(REGISTER_TEMPDATA);
+            adcTemperature <<= 8;
+            adcTemperature |= Read8(REGISTER_TEMPDATA + 2);
+            adcTemperature >>= 4;
+
+            var var1 = ((((adcTemperature >> 3) - (m_Calibration.T1 << 1))) *
+                m_Calibration.T2) >> 11;
+            var var2 = (((((adcTemperature >> 4) - m_Calibration.T1) *
+                ((adcTemperature >> 4) - m_Calibration.T1)) >> 12) *
+                m_Calibration.T3) >> 14;
+            m_Normalized = var1 + var2;
+            var temperature = (float)((m_Normalized * 5 + 128) >> 8);
+            return temperature / 100;
+        }
+
+        public double ReadPressure()
+        {
+            int adcPreassure = Read16(REGISTER_PRESSUREDATA);
+            adcPreassure <<= 8;
+            adcPreassure |= Read8(REGISTER_PRESSUREDATA + 2);
+            adcPreassure >>= 4;
+
+            var var1 = (long)m_Normalized - 128000;
+            var var2 = var1 * var1 * m_Calibration.P6;
+            var2 = var2 + (var1 * m_Calibration.P4 << 17);
+            var2 = var2 + ((long)m_Calibration.P4 << 35);
+            var1 = ((var1 * var1 * m_Calibration.P3) >> 8) +
+                ((var1 * m_Calibration.P2) << 12);
+            var1 = ((1 << 47) + var1) * m_Calibration.P1 >> 33;
+
+            if (var1 == 0)
+            {
+                return 0;
+            }
+            var preassure = (long)1048576 - adcPreassure;
+            preassure = (((preassure << 31) - var2) * 3125) / var1;
+            var1 = (m_Calibration.P9 * (preassure >> 13) * (preassure >> 13)) >> 25;
+            var2 = (m_Calibration.P8 * preassure) >> 19;
+            preassure = ((preassure + var1 + var2) >> 8) + (m_Calibration.P7 << 4);
+
+            return preassure / 256;
+        }
+
+        public double ReadAltitude(double seaLevelhPa)
+        {
+            var preassure = ReadPressure();
+            preassure /= 100;
+            var altitude = 44330 * (1.0 - Math.Pow(preassure / seaLevelhPa, 0.1903));
+            return altitude;
+        }
+
+        private void ReadCoefficients()
+        {
+            m_Calibration.T1 = Read16LittleEndian(REGISTER_DIG_T1);
+            m_Calibration.T2 = (short)Read16LittleEndian(REGISTER_DIG_T2);
+            m_Calibration.T3 = (short)Read16LittleEndian(REGISTER_DIG_T3);
+
+            m_Calibration.P1 = Read16LittleEndian(REGISTER_DIG_P1);
+            m_Calibration.P2 = (short)Read16LittleEndian(REGISTER_DIG_P2);
+            m_Calibration.P3 = (short)Read16LittleEndian(REGISTER_DIG_P3);
+            m_Calibration.P4 = (short)Read16LittleEndian(REGISTER_DIG_P4);
+            m_Calibration.P5 = (short)Read16LittleEndian(REGISTER_DIG_P5);
+            m_Calibration.P6 = (short)Read16LittleEndian(REGISTER_DIG_P6);
+            m_Calibration.P7 = (short)Read16LittleEndian(REGISTER_DIG_P7);
+            m_Calibration.P8 = (short)Read16LittleEndian(REGISTER_DIG_P8);
+            m_Calibration.P9 = (short)Read16LittleEndian(REGISTER_DIG_P9);
+        }
+
+        /// <summary>
+        /// Reads 2 bytes from the specified address
+        /// </summary>
+        /// <param name="addr">Register to read from</param>
+        /// <returns>2 byte register value</returns>
+        private ushort Read16(byte addr)
+        {
+            var address = new byte[] { addr };
+            var data = new byte[2];
+
+            m_Device.WriteRead(address, data);
+
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(data);
+            }
+            var result = BitConverter.ToUInt16(data, 0);
+            return result;
+        }
+
+        /// <summary>
+        /// Reads 2 bytes in little endian format from the specified address
+        /// </summary>
+        /// <param name="addr">Register to read from</param>
+        /// <returns>2 byte register value</returns>
+        private ushort Read16LittleEndian(byte addr)
+        {
+            var address = new byte[] { addr };
+            var data = new byte[2];
+
+            m_Device.WriteRead(address, data);
+
+            if (!BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(data);
+            }
+            var result = BitConverter.ToUInt16(data, 0);
+            return result;
+        }
+
+        /// <summary>
+        /// Read 1 byte form the specified address
+        /// </summary>
+        /// <param name="addr">Register to read from</param>
+        /// <returns>1 byte register value</returns>
+        private byte Read8(byte addr)
+        {
+            var address = new byte[] { addr };
+            var data = new byte[1];
+
+            m_Device.WriteRead(address, data);
+
+            return data[0];
+        }
+
+        /// <summary>
+        /// Writes 1 byte to the specified address
+        /// </summary>
+        /// <param name="addr">Register to write to</param>
+        /// <param name="cmd">Command to write</param>
+        private void Write8(byte addr, byte cmd)
+        {
+            var command = new byte[] { addr, cmd };
+            m_Device.Write(command);
+        }
+    }
+}
---
+++ services/tempprovider/StartupTask.cs
@@ -0,0 +1,153 @@
+﻿using Windows.ApplicationModel.Background;
+using Windows.Devices.I2c;
+using System.Threading;
+using System;
+using System.Collections.Generic;
+using uPLibrary.Networking.M2Mqtt;
+using Windows.Devices.Enumeration;
+using uPLibrary.Networking.M2Mqtt.Exceptions;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace tempprovider
+{
+    public sealed class StartupTask : IBackgroundTask, IDisposable
+    {
+        private const string m_DeviceSelector = "I2C1";
+        private const string m_Host = "127.0.0.1";
+        private const string m_TemperatureTopic = "/schuetz/temperature";
+        private const string m_PressureTopic = "/schuetz/preassure";
+        private const byte m_QoS = 1;
+        private const bool m_RetainMessage = true;
+        private const int m_TimerDueTime = 1000;
+        private const int m_TimerInterval = 2000;
+
+        private readonly string m_ClientId = Guid.NewGuid().ToString();
+        private readonly ManualResetEvent m_ShutdownEvent = new ManualResetEvent(false);
+        private readonly Queue<double> m_TemperatureValues = new Queue<double>();
+        private readonly Queue<double> m_PressureValues = new Queue<double>();
+        private MqttClient m_Client = null;
+        private BackgroundTaskDeferral m_Deferral = null;
+        private I2cDevice m_Device = null;
+        private Timer m_Timer = null;
+        private BMP280 m_Sensor = null;
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            m_Deferral = taskInstance.GetDeferral();
+            taskInstance.Canceled += TaskInstanceCanceled;
+
+            // initialize I2C
+            var i2cSettings = new I2cConnectionSettings(BMP280.Address)
+            {
+                BusSpeed = I2cBusSpeed.FastMode,
+                SharingMode = I2cSharingMode.Shared,
+            };
+            var selector = I2cDevice.GetDeviceSelector(m_DeviceSelector);
+            var deviceInformation = await DeviceInformation.FindAllAsync(selector);
+            m_Device = await I2cDevice.FromIdAsync(deviceInformation[0].Id, i2cSettings);
+
+            // initialize sensor
+            m_Sensor = new BMP280(m_Device);
+            m_Sensor.PowerUp();
+
+            // initialize MQTT
+            try
+            {
+                m_Client = new MqttClient(m_Host);
+                m_Client.Connect(m_ClientId);
+            }
+            catch (MqttConnectionException ex)
+            {
+                // ignore connection exception and retry to connect later
+                Debug.WriteLine("Cannot connect to MQTT broker: " + ex.Message);
+                m_Client = null;
+            }
+
+            // start timer
+            m_Timer = new Timer(TemperatureProvider, null, m_TimerDueTime, m_TimerInterval);
+        }
+
+        private void TemperatureProvider(object state)
+        {
+            // read the temperature values
+            var currentTemperature = m_Sensor.ReadTemperature();
+            m_TemperatureValues.Enqueue(currentTemperature);
+            while (m_TemperatureValues.Count > 10)
+            {
+                m_TemperatureValues.Dequeue();
+            }
+            var temperature = m_TemperatureValues.Average(x => x);
+
+            // read the pressure values
+            var currentPressure = m_Sensor.ReadPressure();
+            m_PressureValues.Enqueue(currentPressure);
+            while (m_PressureValues.Count > 10)
+            {
+                m_PressureValues.Dequeue();
+            }
+            var pressure = m_PressureValues.Average(x => x);
+
+            // publish the sensor values
+            try
+            {
+                if (m_Client == null)
+                {
+                    m_Client = new MqttClient(m_Host);
+                }
+                if (!m_Client.IsConnected)
+                {
+                    m_Client.Connect(m_ClientId);
+                }
+                Debug.WriteLine(string.Format("Publishing: temperature={0}, pressure={1}", temperature, pressure));
+                m_Client.Publish(m_TemperatureTopic, Encoding.UTF8.GetBytes(temperature.ToString()), m_QoS, m_RetainMessage);
+                m_Client.Publish(m_PressureTopic, Encoding.UTF8.GetBytes(pressure.ToString()), m_QoS, m_RetainMessage);
+            }
+            catch (MqttConnectionException ex)
+            {
+                // ignore exception
+                Debug.WriteLine("Cannot connect to MQTT broker: " + ex.Message);
+            }
+        }
+
+        private void TaskInstanceCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            m_Deferral.Complete();
+        }
+
+        #region IDisposable Support
+
+        private bool disposedValue = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if (m_Timer != null)
+                    {
+                        m_Timer.Dispose();
+                    }
+                    if (m_Device != null)
+                    {
+                        m_Device.Dispose();
+                    }
+                    m_ShutdownEvent.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        #endregion IDisposable Support
+    }
+}
