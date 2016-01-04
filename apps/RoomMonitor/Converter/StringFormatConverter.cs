1b72e657355abffe337f841d40214912964d3a13
LokiFaun <johannes.schuetz@outlook.com>
2016-01-04 14:25:59 +0100

    usage of user defined StringFormatter
    changed: apps\RoomMonitor\App.xaml - moved global resources to
    application
    removed: apps\RoomMonitor\Converter\DateConverter - handled using
    StringFormatter
    removed: apps\RoomMonitor\Converter\TimeConverter - handled using
    StringFormatter
    changed: apps\RoomMonitor\ViewModel\MainViewModel - added properties for
    current lux, pressure and temperature
    changed: apps\RoomMonitor\View\MainPage - display current lux, pressure
    and temperature

---
+++ apps/RoomMonitor/Converter/StringFormatConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace Dashboard.Converter
+{
+    internal class StringFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            if (parameter == null)
+            {
+                return null;
+            }
+
+            return string.Format(parameter as string, value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- apps/RoomMonitor/ViewModel/MainViewModel.cs
+++ apps/RoomMonitor/ViewModel/MainViewModel.cs
@@ -1,15 +1,24 @@
 ﻿namespace Dashboard.ViewModel
 {
-    using System;
-
     using Logic;
+    using System;
 
     internal class MainViewModel : ViewModel
     {
         /// <summary>
+        /// The name of the MainViewModel.
+        /// </summary>
+        public const string Name = "Main";
+
+        /// <summary>
         /// The IoC container
         /// </summary>
         private readonly Container m_Container;
+
+        /// <summary>
+        /// The current altitude
+        /// </summary>
+        private double m_CurrentAltitude;
 
         /// <summary>
         /// The current date/time
@@ -17,9 +26,19 @@
         private DateTime m_CurrentDateTime;
 
         /// <summary>
-        /// The name of the MainViewModel.
+        /// The current luminance
         /// </summary>
-        public const string Name = "Main";
+        private double m_CurrentLuminance;
+
+        /// <summary>
+        /// The current pressure
+        /// </summary>
+        private double m_CurrentPressure;
+
+        /// <summary>
+        /// The current temperature
+        /// </summary>
+        private double m_CurrentTemperature;
 
         /// <summary>
         /// Initializes an instance of <see cref="MainViewModel"/>
@@ -29,6 +48,22 @@
         {
             m_Container = container;
             CurrentDateTime = DateTime.Now;
+            CurrentTemperature = 21.5;
+            CurrentPressure = 1025.5;
+            CurrentLuminance = 3000;
+        }
+
+        /// <summary>
+        /// Gets or sets the current altitude
+        /// </summary>
+        public double CurrentAltitude
+        {
+            get { return m_CurrentAltitude; }
+            set
+            {
+                m_CurrentAltitude = value;
+                NotifyPropertyChanged(nameof(CurrentAltitude));
+            }
         }
 
         /// <summary>
@@ -41,6 +76,45 @@
             {
                 m_CurrentDateTime = value;
                 NotifyPropertyChanged(nameof(CurrentDateTime));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the current luminance
+        /// </summary>
+        public double CurrentLuminance
+        {
+            get { return m_CurrentLuminance; }
+            set
+            {
+                m_CurrentLuminance = value;
+                NotifyPropertyChanged(nameof(CurrentLuminance));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the current pressure
+        /// </summary>
+        public double CurrentPressure
+        {
+            get { return m_CurrentPressure; }
+            set
+            {
+                m_CurrentPressure = value;
+                NotifyPropertyChanged(nameof(CurrentPressure));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the current temperature
+        /// </summary>
+        public double CurrentTemperature
+        {
+            get { return m_CurrentTemperature; }
+            set
+            {
+                m_CurrentTemperature = value;
+                NotifyPropertyChanged(nameof(CurrentTemperature));
             }
         }
     }
